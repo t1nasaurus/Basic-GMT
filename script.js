9f812893ce60329b48028476d5a82dda144193cf
liempot <leemagno9@gmail.com>
2024-03-14 14:57:04 +0800

    header 100%

---
+++ script.js
@@ -0,0 +1,9 @@
+const burgir = document.querySelector(".burgir__menu")
+const hNaV = document.querySelector(".header__nav")
+burgir.addEventListener("click", () =>{
+    burgir.classList.toggle("open");
+    hNaV.classList.toggle("open");
+    
+    
+
+})
